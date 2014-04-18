1ce40e215b00b2f6d4a6eaefb211df4f8e402642
Cezary Wojcik <cezarywojcik92@gmail.com>
2014-04-17 21:03:15 -0700

    Initial Commit

---
+++ CorvallisBusApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CorvallisBusApp
+//
+//  Created by Cezary Wojcik on 4/17/14.
+//  Copyright (c) 2014 Corvallis Bus App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CorvallisBusApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CorvallisBusApp
+//
+//  Created by Cezary Wojcik on 4/17/14.
+//  Copyright (c) 2014 Corvallis Bus App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
