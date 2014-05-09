1a847d3764a58ba749fd6f82729932050cacdd51
Russell Barnes <russell.barnes.1@gmail.com>
2014-05-09 00:51:53 -0700

    getStopsForRoute

--- CorvallisBusApp/Stops.m
+++ CorvallisBusApp/Stops.m
@@ -7,6 +7,7 @@
 //
 
 #import "Stops.h"
+#import "Routes.h"
 
 @implementation Stops
 
@@ -73,6 +74,43 @@
     //      [[[stops objectAtIndex:0] objectForKey:@"Long"] floatValue]);
     
     return stops;
+}
+
++(NSArray *)getStopsForRoute:(NSString *)route
+{
+    // Get list of stops for route
+    NSString *requesturl = [NSString stringWithFormat:@"http://www.corvallis-bus.appspot.com/routes?stops=true&names=%@", route];
+    
+    NSDictionary *routeWithStops = nil;
+    
+    NSURL *url = [NSURL URLWithString:requesturl];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLResponse *response;
+    NSError *error;
+    
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    if (error) {
+        NSLog(@"ERROR in getStopsForRoute.  Response from server = %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+        return nil;
+    }
+    
+    NSError *jsonError;
+    routeWithStops = [[[NSJSONSerialization JSONObjectWithData:data
+                                              options:NSJSONReadingAllowFragments
+                                                error:&jsonError] objectForKey:@"routes"] objectAtIndex:0];
+    if (jsonError != nil) {
+        NSLog(@"JSON error while parsing routes from server in getStopsForRoute");
+        return nil;
+    }
+    
+    // This is how to access stop data:
+    //NSLog(@"First route - Name: %@ - Description: %@",
+    //      [[[self.routeWithStops objectForKey:@"Path] objectAtIndex:0] objectForKey:@"Lat"]
+    
+    // Return dictionary of all stops for this route
+    return [routeWithStops objectForKey:@"Path"];
+       
 }
 
 @end
